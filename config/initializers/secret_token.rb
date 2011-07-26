9ec833d1138cbc998e32ad044cfb75491021d884
pgturd2 <powerdump556@hotmail.com>
2011-07-25 22:25:32 -0700

    first duplicate of sample_app_ubuntu

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleAppUbuntu::Application.config.secret_token = '641b282d1fac59f257a81330826a77ced088fff1728ae9e4d169b3c441ad1a3abebab96979aa930d6003d2496a3dd8b60fbb00fde18185f6f758751797790ade'
